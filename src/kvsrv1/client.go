d4beb84cbc927d2c67d127236f81666010f7a5c2
Beshr Islam Bouli <besherislambouley@gmail.com>
2025-02-21 20:41:20 -0500

    Key/value server with dropped messages

--- src/kvsrv1/client.go
+++ src/kvsrv1/client.go
@@ -4,6 +4,7 @@ import (
 	"6.5840/kvsrv1/rpc"
 	"6.5840/kvtest1"
 	"6.5840/tester1"
+	"time"
 )
 
 
@@ -29,20 +30,25 @@ func MakeClerk(clnt *tester.Clnt, server string) kvtest.IKVClerk {
 // must match the declared types of the RPC handler function's
 // arguments. Additionally, reply must be passed as a pointer.
 func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
-	args := rpc.GetArgs {
-		Key: key,
-	}
 
-	reply := rpc.GetReply {}
-
-	ok := ck.clnt.Call(ck.server, "KVServer.Get", &args, &reply)
-	if !ok {
-		panic("Get failed")
+	for true {
+		args := rpc.GetArgs {
+			Key: key,
+		}
+	
+		reply := rpc.GetReply {}
+	
+		ok := ck.clnt.Call(ck.server, "KVServer.Get", &args, &reply)
+		if !ok {
+			time.Sleep(100 * time.Millisecond)
+			continue
+		}
+		if reply.Err == rpc.ErrNoKey {
+			return "", 0, reply.Err
+		}
+		return reply.Value, reply.Version, reply.Err
 	}
-	if reply.Err == rpc.ErrNoKey {
-		return "", 0, reply.Err
-	}
-	return reply.Value, reply.Version, reply.Err
+	panic ("should not happen")
 }
 
 // Put updates key with value only if the version in the
@@ -64,17 +70,25 @@ func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
 // arguments. Additionally, reply must be passed as a pointer.
 func (ck *Clerk) Put(key, value string, version rpc.Tversion) rpc.Err {
 	// You will have to modify this function.
-	args := rpc.PutArgs {
-		Key: key,
-		Value: value,
-		Version: version,
-	}
-	reply := rpc.PutReply {}
-
-	ok := ck.clnt.Call(ck.server, "KVServer.Put", &args, &reply)
-	if !ok {
-		panic("Put failed")
-	}
+	t := 0 
+	for true {
+		t ++
+		args := rpc.PutArgs {
+			Key: key,
+			Value: value,
+			Version: version,
+		}
+		reply := rpc.PutReply {}
 	
-	return reply.Err
+		ok := ck.clnt.Call(ck.server, "KVServer.Put", &args, &reply)
+		if !ok {
+			time.Sleep(100 * time.Millisecond)
+			continue
+		}
+		if t > 1 && reply.Err == rpc.ErrVersion {
+			return rpc.ErrMaybe
+		}
+		return reply.Err
+	}
+	panic ("should not happen")
 }
